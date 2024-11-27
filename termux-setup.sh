20ce553c6c0456771e1441a7b173a27ca18b3a91
drksch <163310407+drksch@users.noreply.github.com>
2024-11-27 11:58:44 +0800

    font and script

---
+++ termux-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##1. change repo
+echo Did you set the repo
+##2. Foreword update && upgrade -y
+echo Updating packages
+##3. 1st Chapter: A fresh Start!
+## pkg install neovim git gh
