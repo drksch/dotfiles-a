1b8954e48096a6ed6e5a2e2446ca05929056efff
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2024-11-30 07:37:50 +0800

    Update freshstart.sh

--- freshstart.sh
+++ freshstart.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 echo "It seems that you have been caught by Truck-kun. You have to choose the new world you want to live in. Choose wisely!"
-	select os in "Linux" "Termux" "Windows" "MacOS" "Give me a moment"; do
-		case $os in
-			"Linux")
-				bash ./setup/linux-setup.sh
-				;;
-			"Termux")
-				bash ./setup/termux-setup.sh
-				;;
-			"Windows")
-				bash ./setup/windows-setup.sh
-				;;
-			"MacOS")
-				echo "MacOS is not supported yet"
-				;;
-			"Give me a moment")
-				echo "Time wait for no man, even if he is just a spirit."
-				;;
-		esac
-		break
+	select os in "Linux" "Arch-Mini" "Termux" "Windows" "MacOS" "Give me a moment"; do
+	  case $os in
+	   "Linux")
+	     bash ./setup/linux-setup.sh
+	     ;;
+	   "Termux")
+	     bash ./setup/termux-setup.sh
+	     ;;
+	   "Windows")
+	     bash ./setup/windows-setup.sh
+	     ;;
+	   "Arch-Mini")
+	     bash ./setup/archmini-setup.sh
+	     ;;
+	   "MacOS")
+	     echo "MacOS is not supported yet"
+	     ;;
+	   "Give me a moment")
+	     echo "Time wait for no man, even if he is just a spirit."
+	     ;;
+	 esac
+	 break
 	done
 
 
