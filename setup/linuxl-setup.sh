b0721f2ab8beac1925797dfd3773680199f0b8c4
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2024-12-02 06:12:46 +0800

    Update linuxl-setup.sh

--- setup/linuxl-setup.sh
+++ setup/linuxl-setup.sh
@@ -178,18 +178,16 @@ select fish_choice in "Yes" "No"; do
     case $fish_choice in
         "Yes")
             # Install Fish, Starship, Tealdeer, and Fastfetch
-            if sudo pacman -Sy fish starship tldr-go-client-git fastfetch pay-respects; then
-                echo -e ${DM} "Fish, Starship, tldr, and Fastfetch installed successfully."${NC}
+            if sudo pacman -S --noconfirm --needed fish starship tldr-go-client-git fastfetch pay-respects; then
+                echo -e ${DM}"Fish, Starship, tldr, and Fastfetch installed successfully."${NC}
             else
-                echo -e ${DM} "Error installing Fish, Starship, tldr, and Fastfetch. Please check the package manager output for more information."${NC}
+                echo -e ${DM}"Error installing Fish, Starship, tldr, and Fastfetch. Please check the package manager output for more information."${NC}
             fi
-            done
             ;;
         "No")
             echo -e ${DM} "Fish shell not installed."${NC}
             ;;
     esac
-    break
 done
 
 #"Setting Environment Variables"
