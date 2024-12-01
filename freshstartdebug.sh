fe650124da0fd26c59942a7d198c25580415d9d1
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2024-12-02 05:14:06 +0800

    Update freshstartdebug.sh

--- freshstartdebug.sh
+++ freshstartdebug.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Current working directory: $(pwd)"
+installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
+
 # Start
 echo -e "\033[1mInsperata accidunt magis saepe quam quae speres"
 sleep 1
@@ -24,46 +26,46 @@ select os in \
   "Give me a moment..." ; do
   case $os in
     "Arch-Heavy")
-      echo "Running script: ./setup/linuxh_setup.sh"
-      if [ -f "setup/linuxh_setup.sh" ]; then
+      echo "Running script: $installed_dir/setup/linuxh_setup.sh"
+      if [ -f "$installed_dir/setup/linuxh_setup.sh" ]; then
         echo "Script found, running..."
-        bash setup/linuxh_setup.sh
+        bash $installed_dir/setup/linuxh_setup.sh
       else
         echo "Error: Script not found!"
       fi
       ;;
     "Arch-Light")
-      echo "Running script: ./setup/linuxl_setup.sh"
-      if [ -f "setup/linuxl_setup.sh" ]; then
+      echo "Running script: $installed_dir/setup/linuxl_setup.sh"
+      if [ -f "$installed_dir/setup/linuxl_setup.sh" ]; then
         echo "Script found, running..."
-        bash setup/linuxl_setup.sh
+        bash $installed_dir/setup/linuxl_setup.sh
       else
         echo "Error: Script not found!"
       fi
       ;;
     "Termux-Android")
-      echo "Running script: ./setup/termux-setup.sh"
-      if [ -f "setup/termux-setup.sh" ]; then
+      echo "Running script: $installed_dir/setup/termux-setup.sh"
+      if [ -f "$installed_dir/setup/termux-setup.sh" ]; then
         echo "Script found, running..."
-        bash setup/termux-setup.sh
+        bash $installed_dir/setup/termux-setup.sh
       else
         echo "Error: Script not found!"
       fi
       ;;
     "Windows")
-      echo "Running script: ./setup/windows-setup.sh"
-      if [ -f "setup/windows-setup.sh" ]; then
+      echo "Running script: $installed_dir/setup/windows-setup.sh"
+      if [ -f "$installed_dir/setup/windows-setup.sh" ]; then
         echo "Script found, running..."
-        bash setup/windows-setup.sh
+        bash $installed_dir/setup/windows-setup.sh
       else
         echo "Error: Script not found!"
       fi
       ;;
     "MacOS")
-      echo "Running script: ./setup/doyouhavemacmoney.sh"
-      if [ -f "setup/doyouhavemacmoney.sh" ]; then
+      echo "Running script: $installed_dir/setup/doyouhavemacmoney.sh"
+      if [ -f "$installed_dir/setup/doyouhavemacmoney.sh" ]; then
         echo "Script found, running..."
-        bash setup/doyouhavemacmoney.sh
+        bash $installed_dir/setup/doyouhavemacmoney.sh
       else
         echo "Error: Script not found!"
       fi
