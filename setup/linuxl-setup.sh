5463988f76cda167c9d8e6ab8984bca472105bec
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2025-03-25 02:10:06 +0800

    Update linuxl-setup.sh

--- setup/linuxl-setup.sh
+++ setup/linuxl-setup.sh
@@ -42,8 +42,8 @@ echo -e ${DM}"ChaoticAur?"${NC}
 select chaotic_aur in "Yes" "No"; do
     case $chaotic_aur in
         "Yes")
-            sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com --noconfirm
-            sudo pacman-key --lsign-key 3056513887B78AEB --noconfirm
+            sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+            sudo pacman-key --lsign-key 3056513887B78AEB
             sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
             sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
             sleep 2
